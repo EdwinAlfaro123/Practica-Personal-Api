d731e72ef000a93d6f5aedc11712afcccf2a2ed5
EdwinAlfaro123 <20210300@ricaldone.edu.sv>
2025-08-02 00:17:11 -0600

    primer commit

---
+++ src/main/java/PracticaPersonal2/PracticaPersonal2/PracticaPersonal2Application.java
@@ -0,0 +1,13 @@
+package PracticaPersonal2.PracticaPersonal2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaPersonal2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaPersonal2Application.class, args);
+	}
+
+}
---
+++ src/main/java/PracticaPersonal2/PracticaPersonal2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package PracticaPersonal2.PracticaPersonal2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PracticaPersonal2Application.class);
+	}
+
+}
